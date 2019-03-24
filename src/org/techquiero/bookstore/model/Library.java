ac2f847d4575c11bc23520b4cf92633002dd7442
Auley03 <auley.03@gmail.com>
2019-03-24 16:29:49 -0600

    Final changes

--- src/org/techquiero/bookstore/model/Library.java
+++ src/org/techquiero/bookstore/model/Library.java
@@ -1,8 +1,36 @@
 package org.techquiero.bookstore.model;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Library {
 
+	private List<Book> miBooksito = null;
+	
+	public Library(){
+		if(miBooksito == null) {
+			miBooksito = new ArrayList<Book>();
+		
+			miBooksito.add(new Book(1, "Microservicios", "Christian Posta", 45.98));
+			miBooksito.add(new Book(2, "Patrones de diseño", "Markus Eisele", 72.80));
+			miBooksito.add(new Book(3, "Programación Orientada a Objetos", "Richard Warbuton", 36.72));
+			miBooksito.add(new Book(5, "Aprendiendo Python", "David Mertz", 43.27));
+			miBooksito.add(new Book(6, "Big Data con Hadoop", "Donald Miner", 39.60));
+			miBooksito.add(new Book(7, "IOS con Swift", "Tim Nugent", 24.18));
+			miBooksito.add(new Book(8, "Programación reactiva", "Konrad Malawski", 58.70));
+			miBooksito.add(new Book(9, "Programación con C", "Joel Falcou", 67.93));
+			miBooksito.add(new Book(10, "Diseño de sistemas", "Doug Davis", 55.67));
+		
+		}
+		
+	}
 
+	public List<Book> getMiBooksito() {
+		return miBooksito;
+	}
+
+	public void setMiBooksito(List<Book> miBooksito) {
+		this.miBooksito = miBooksito;
+	}
 	
 }
--- src/org/techquiero/bookstore/test/Test.java
+++ src/org/techquiero/bookstore/test/Test.java
@@ -1,28 +1,44 @@
 package org.techquiero.bookstore.test;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import org.techquiero.bookstore.model.Book;
+import org.techquiero.bookstore.model.Library;
+
 import java.util.Scanner;
 
 public class Test {
 
 	public static void main(String[] args) {
-		
+
 		Scanner entrance = new Scanner(System.in);
 		int option = 0;
+
+		Library library= new Library(); 
+
+		List<Book> shoppingCart = new ArrayList<Book>();
 		
-		List<Book> library = new ArrayList<Book>();
-		library.add(new Book(1, "Microservicios", "Christian Posta", 45.98));
-		library.add(new Book(2, "Patrones de diseño", "Markus Eisele", 72.80));
-		library.add(new Book(3, "Programación Orientada a Objetos", "Richard Warbuton", 36.72));
-		library.add(new Book(4, "Aprendiendo Java", "Ben Evans", 66.81));
-		library.add(new Book(5, "Aprendiendo Python", "David Mertz", 43.27));
-		library.add(new Book(6, "Big Data con Hadoop", "Donald Miner", 39.60));
-		library.add(new Book(7, "IOS con Swift", "Tim Nugent", 24.18));
-		library.add(new Book(8, "Programación reactiva", "Konrad Malawski", 58.70));
-		library.add(new Book(9, "Programación con C", "Joel Falcou", 67.93));
-		library.add(new Book(10, "Diseño de sistemas", "Doug Davis", 55.67));
+		String ruta = "TotalBill.txt";
+		File file = new File(ruta);
+		
+		BufferedWriter bw = null;
+		
+		try {
+		
+		if (!file.exists()) {
+            file.createNewFile();
+        }
+
+		FileWriter fw = new FileWriter(file);
+		bw = new BufferedWriter(fw);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 		
 		do {
 			System.out.println("Welcome to TechQuiero Bookstore");
@@ -33,39 +49,88 @@ public class Test {
 			System.out.println("4) Confirm buy");
 			System.out.println("5) Exit");
 			option = entrance.nextInt();
-			
+
 			switch(option) {
 			case 1: 
 				System.out.println("Show books");
-				for(Book book : library) {
+				for(Book book : library.getMiBooksito()) {
 					System.out.println(book.toString());
 				}
 				break;	
 			case 2: 
 				System.out.print("How many books would you like to buy?");
 				int booksUser = entrance.nextInt();
-				for(Book book : library) {
+				for(Book book : library.getMiBooksito()) {
 					System.out.println(book.toString());
 				}
 				for(int i= 0; i<booksUser; i++) {
 					System.out.print("\nPlease enter the id of the book you would like to buy: ");
-					library.add(library.get(entrance.nextInt()-1));
+					int idBook = entrance.nextInt();
+					System.out.print("\nDo you confirm that you want this book? Yes / No");
+					entrance.nextLine();
+					String confirmBook = entrance.nextLine();
+					if(confirmBook.equalsIgnoreCase("YES")) {
+						shoppingCart.add(library.getMiBooksito().get(idBook-1));
+					}
+					else if(confirmBook.equalsIgnoreCase("NO")) {
+
+					}
+					else {
+						System.out.println("Please select a valid option");
+					}
 				}
-			
+				for(Book book: shoppingCart)
+					try {
+						bw.write(book.toString() + "\n");
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				System.out.println("Working Directory = " + System.getProperty("user.dir") + '/' + ruta);
 				break;	
 			case 3: 
-				
+				String finishRem = "";
+				for(Book miBook: shoppingCart){
+				System.out.println(miBook.toString());
+			}
+			do {
+				System.out.print("\nPlease enter the id's of the books that you want to remove: ");
+				int removeBook = entrance.nextInt();
+				shoppingCart.remove(removeBook-1);
+				if(!shoppingCart.isEmpty()) {
+					System.out.print("\nDo you want to remove antoher book: Yes / No");
+					entrance.nextLine();
+					finishRem = entrance.nextLine();
+				}
+				else {
+					finishRem = "NO";
+				}
+			}
+			while(finishRem.equalsIgnoreCase("YES"));
 				break;	
 			case 4: 
-				
+				for(Book miBook: shoppingCart){
+				System.out.println(miBook.toString());
+
+			}
 				break;
 			case 5: 
-				
+				System.out.print("Enjoy your books!");
+				break;	
+
+			default:
+				System.out.print("The option is not available");
 				break;	
 			}
 
-		}while(option != 5);
-		
+		}	
+		while(option != 5);
+
+		entrance.close();
+		try {
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
